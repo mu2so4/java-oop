6a48260a786324b839d654809649d51475259ea7
mu2so4 <b4.orangutang@gmail.com>
2022-02-20 14:42:14 +0700

    fixed entering from terminal

--- lab1/Main.java
+++ lab1/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String inputFileName, outputFileName;
-        if(args.length != 3) {
+        if(args.length != 2) {
             Scanner scanner = new Scanner(System.in);
             inputFileName = scanner.next();
             outputFileName = scanner.next();
             scanner.close();
         }
         else {
-            inputFileName = args[1];
-            outputFileName = args[2];
+            inputFileName = args[0];
+            outputFileName = args[1];
         }
         Dictionary dictionary = new Dictionary();
         DictionaryReader reader = new DictionaryReader(inputFileName);
